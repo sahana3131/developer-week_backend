f9d240b562e7059e5fa3c6406d6f93c55e3ac0e8
Amirthavarshini V <96651600+Amirtha2503@users.noreply.github.com>
2023-02-15 21:02:49 +0530

    Add files via upload

--- FastAPI_Backend/main.py
+++ FastAPI_Backend/main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI, Body, Depends, HTTPException
 from app.model import PostSchema, UserSchema, UserLoginSchema
 from app.auth.auth_bearer import JWTBearer
 from app.auth.auth_handler import signJWT
-
+import httpx
+from starlette.responses import RedirectResponse
 
 giturl = [
     {
@@ -26,7 +27,8 @@ giturl = [
 users = []
 
 app = FastAPI()
-
+github_client_id = 'b0a94e000593ed9730a0'
+github_client_secret = '6a996f48c85b58803ccbd4674fdd87bc468b195f'
 
 
 def check_user(data: UserSchema):
@@ -72,6 +74,27 @@ def add_post(post: PostSchema):
         "data": "post added."
     }
 
+@app.get('/github-login')
+async def github_login():
+    return RedirectResponse(f'https://github.com/login/oauth/authorize?client_id={github_client_id}', status_code=302)
+
+@app.get('/github-code')
+async def github_code(code: str):
+    params = {
+            'client_id' : github_client_id,
+            'client_secret' : github_client_secret,
+            'code' : code
+        }
+    headers = {'Accept': 'application/json'}
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url='https://github.com/login/oauth/access_token',params=params, headers=headers)
+    response_json = response.json()
+    access_token=response_json['access_token']
+    async with httpx.AsyncClient() as client:
+        headers.update({'Authorization': f'Bearer {access_token}'})
+        response = await client.get('https://api.github.com/user', headers=headers)
+    return response.json() 
+
 # without db
 @app.post("/user/signup", tags=["user"])
 def create_user(user: UserSchema = Body(...)):
@@ -86,4 +109,6 @@ def user_login(user: UserLoginSchema = Body(...)):
     return {
         "error": "Wrong login details!"
     }
+
+
 
